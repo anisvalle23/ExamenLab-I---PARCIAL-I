06bc376afec72ad61b2c0d70bfb888b4f8f42bf6
Ana Valle <anavalle23@unitec.edu>
2024-08-16 14:36:55 -0600

    update

--- ExamenLab I - PARCIAL I/src/Comment.java
+++ ExamenLab I - PARCIAL I/src/Comment.java
@@ -1,4 +1,40 @@
 
-public class Comment {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+public final class Comment {
+
+    int postId;
+    String autor;
+    String contenido;
+    Calendar fecha;
+
+    public Comment(int postId, String autor, String contenido) {
+        this.postId = postId;
+        this.autor = autor;
+        this.contenido = contenido;
+        fecha = Calendar.getInstance();
+    }
+
+    public String print() {
+        return "   " + autor + "- " + new SimpleDateFormat("dd/MM/yy").format(fecha.getTime()) + "\n  " + contenido;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public Calendar getFecha() {
+        return fecha;
+    }
 
 }
--- ExamenLab I - PARCIAL I/src/Commentable.java
+++ ExamenLab I - PARCIAL I/src/Commentable.java
@@ -1,5 +1,5 @@
 
-
 public interface Commentable {
-    
+
+    boolean addComent(Comment comment);
 }
